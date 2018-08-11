df408fcccb0de504e17c831a34dcdbfd5ee75c3f
Timmy <3531291254@qq.com>
2018-08-11 09:51:47 +0800

    change

--- yinyon_return/test.cpp
+++ yinyon_return/test.cpp
@@ -1,6 +1,38 @@
-#include <iostrem>
+#include <iostream>
 using namespace std;
 
 double val[]={10.1,12.6,33.1,24.1,43.0};
+
+double& setvalue(int i)
+{
+	return val[i];
+}
+
+int main()
+{
+	cout<<"改变前的值: "<<endl;
+	for(int i=0; i<5; i++)	
+	{
+		cout<<"val["<<i<<"]=";
+		cout<<val[i]<<endl;
+	}
+
+
+	setvalue(1) = 100.0;
+	setvalue(3) = 200.0;
+
+	cout<<"改变后的值: " <<endl;
+
+	for(int i=0; i<5; i++)
+	{
+		cout<<"val["<<i<<"]=";
+		cout<<val[i]<<endl;
+	}
+
+	return 0;
+}
+
+
+
 
 
