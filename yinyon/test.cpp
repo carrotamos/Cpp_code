437e15586e6de7e853c8fa0e406658c50f75cb4f
Timmy <3531291254@qq.com>
2018-08-11 09:37:10 +0800

    change

---
+++ yinyon/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i;
+	double d;
+	
+	int& r = i;
+	double& s = d;
+	
+	i = 5;
+	cout << "value of i: "<< i<<endl;
+	cout << "value of i reference: "<<r<<endl;
+	
+	d= 11.3;
+
+	cout << "value of d: "<<d<<endl;
+	cout <<"value of d reference: "<<s<<endl;
+
+	return 0;
+ 
+}
