5d1e0ad0060c2d75bdde1e8a1af5d2ee81b71f4d
carrotamos <3531291254@qq.com>
2018-09-04 08:28:17 +0800

    s

---
+++ cpp_primer_practice/10_7/use_stack.cpp
@@ -0,0 +1,53 @@
+#include"stack.h"
+int main()
+{
+	using namespace std;
+	Stack st;
+	char ch;
+	unsigned long po;
+	cout<<"Please enter A to add a purchase order"<<endl
+		<<"P to process a PO,or Q to quit."<<endl;
+	while(cin>>ch && toupper(ch)!='Q')
+	{
+		while(cin.get() != '\n')
+		{
+			continue;
+		}
+		if(!isalpha(ch))
+		{
+			cout<<"\a";
+			continue;
+		}
+		switch(ch)
+		{
+			case 'A':
+			case 'a': cout<<"Enter a PO number to add";
+					  cin>> po;
+					  if(st.isfull())
+					  {
+						  cout<<"stack already full"<<endl;
+					  }
+					  else
+					  {
+						  st.push(po);
+					  }
+					  break;
+			case 'P':
+			case 'p': if(st.isempty())
+					  {
+						  cout<<"stack already empty"<<endl;
+					  }
+					  else
+					  {
+						  st.pop(po);
+						  cout<<"PO #"<<po<<"poped"<<endl;
+					  }
+					  break;
+		}
+		cout<<"PLease enter A to add a purchase order"<<endl
+			<<"P to process a po, or Q to quit"<<endl;
+	}
+
+	cout<<"Bye"<<endl;
+	return 0;
+}
